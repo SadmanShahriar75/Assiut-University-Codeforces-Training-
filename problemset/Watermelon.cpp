f833fc61cbf266dab6a30a09ea2623b24b163e8f
SadmanShahriar75 <sadmanshahriar7576@gmail.com>
2024-07-13 15:51:24 +0600

    hi

---
+++ problemset/Watermelon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long X;
+    cin >> X ;
+    if (X%2==0 && X> 2){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+
+
+
+    return 0;
+}
